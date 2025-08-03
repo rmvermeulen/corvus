dd5ffae3179353cddf136e4106754bed3b7fe20b
Rasmus <rasmus_vermeulen@tutanota.com>
2025-08-03 17:28:48 +0200

    build.rs: recursively walk assets for *.cob files

--- build.rs
+++ build.rs
@@ -1,31 +1,90 @@
-use std::fs::{self, DirEntry};
-use std::io;
+use std::fs::DirEntry;
+use std::path::PathBuf;
 use std::process::Command;
+use std::{fs, io};
+
+type Line = (String, String);
+
+fn cobble<P: Into<PathBuf>>(path: P, prefix: Option<String>) -> impl IntoIterator<Item = Line> {
+    let path: PathBuf = path.into();
+    if path.is_file() {
+        return Vec::new();
+    }
+
+    fn process_dir(entry: DirEntry, prefix: Option<String>) -> impl IntoIterator<Item = Line> {
+        let dir_name = entry
+            .path()
+            .file_name()
+            .unwrap()
+            .to_string_lossy()
+            .to_string();
+        let prefix = prefix
+            .map(|prefix| format!("{prefix}_{dir_name}"))
+            .unwrap_or(dir_name);
+        cobble(entry.path(), Some(prefix))
+    }
+
+    fn process_file(name: &std::ffi::OsStr, entry: DirEntry, prefix: Option<String>) -> Line {
+        let path = entry
+            .path()
+            .iter()
+            .skip(1)
+            .collect::<PathBuf>()
+            .to_string_lossy()
+            .to_string();
+        let name = name.to_string_lossy().to_string();
+        let name = prefix
+            .map(|prefix| format!("{prefix}_{name}"))
+            .unwrap_or(name);
+        (path, name)
+    }
+
+    fn process_entry(
+        name: &std::ffi::OsStr,
+        entry: fs::DirEntry,
+        prefix: Option<String>,
+    ) -> Option<Vec<Line>> {
+        let ft = entry.file_type().ok()?;
+        if ft.is_file() && entry.path().extension().is_some_and(|ext| ext == "cob") {
+            Some(vec![process_file(name, entry, prefix)])
+        } else if ft.is_dir() {
+            let x = process_dir(entry, prefix);
+            Some(x.into_iter().collect())
+        } else {
+            None
+        }
+    }
+
+    let entries = fs::read_dir(path)
+        .map(|entries| entries.flatten().collect())
+        .unwrap_or_else(|_| Vec::new());
+
+    entries
+        .into_iter()
+        .filter_map(|entry| {
+            let path = entry.path();
+            let name = path.file_stem()?;
+            process_entry(name, entry, prefix.clone())
+        })
+        .flatten()
+        .collect::<Vec<_>>()
+}
 
 fn generate_cobweb_manifest(manifest_path: &str) -> std::io::Result<()> {
     let current_manifest = fs::read_to_string(manifest_path).expect("Failed to read manifest.cob");
 
-    let mut names_to_import = fs::read_dir("assets")?
-        .collect::<Vec<_>>()
+    let entries = cobble("assets", None).into_iter().collect::<Vec<_>>();
+
+    let mut names_to_import = entries
         .into_iter()
-        .filter_map(|res| res.ok())
-        .filter(|entry: &DirEntry| {
-            entry.file_type().is_ok_and(|ft| ft.is_file())
-                && entry.path().extension().is_some_and(|ext| ext == "cob")
-        })
-        .filter_map(|entry| {
-            let name = entry.file_name();
-            let name = name.to_string_lossy();
-            let name = name.strip_suffix(".cob")?;
-            // do not include the manifest inside itself
-            (name != "manifest").then(|| name.to_string())
-        })
+        // do not include the manifest inside itself
+        .filter(|(_, name)| name != "manifest")
         .collect::<Vec<_>>();
 
     names_to_import.sort();
     let imports = names_to_import
         .into_iter()
-        .map(|name| format!(r#""{name}.cob" as {name}"#));
+        .map(|(path, name)| format!(r#""{path}" as {name}"#));
 
     let manifest = ["#manifest".to_string()]
         .into_iter()
