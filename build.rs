503fed1fc90df4a6775cd2aa9afefa19fe70c565
Rasmus <rasmus_vermeulen@tutanota.com>
2025-08-06 15:53:09 +0200

    build.rs+cobble: refactor, inspect manifest entries

--- build.rs
+++ build.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 use std::process::Command;
 use std::{fs, io};
 
-use cobble::generate_cobweb_manifest;
+use cobble::{get_cobweb_entries, get_cobweb_manifest};
 
 fn configure_hooks_path(path: &str) -> io::Result<bool> {
     Command::new("git")
@@ -11,24 +11,34 @@ fn configure_hooks_path(path: &str) -> io::Result<bool> {
         .map(|code| code.success())
 }
 
-fn update_cobweb_manifest<P: Into<PathBuf>>(manifest_path: P) -> io::Result<()> {
+fn update_manifest_if_changed<P: Into<PathBuf>>(
+    manifest_path: P,
+    manifest: String,
+) -> io::Result<()> {
     let manifest_path = manifest_path.into();
-    let manifest = generate_cobweb_manifest(&manifest_path);
-    if let Ok(current_manifest) = fs::read_to_string(&manifest_path)
-        && current_manifest == manifest
-    {
+    let is_manifest_up_to_date = fs::read_to_string(&manifest_path)
+        .is_ok_and(|current_manifest| current_manifest == manifest);
+
+    if is_manifest_up_to_date {
         Ok(())
     } else {
         fs::write(&manifest_path, manifest.as_bytes())
     }
 }
 
-// TODO: generate embed-macros for manifest files in release mode
-
 fn main() {
     assert!(
         configure_hooks_path("hooks").unwrap(),
         "configure_hooks_path: unexpected exit code"
     );
-    update_cobweb_manifest("assets/cobweb/manifest.cob").expect("update_cobweb_manifest failed");
+
+    let cobweb_entries =
+        get_cobweb_entries("assets/cobweb/", None).expect("get_cobweb_entries failed");
+
+    // TODO: generate embed-macros for manifest files in release mode
+    // for e in cobweb_entries { bevy_cobweb_UI::load_embedded_scene_file }
+
+    let manifest = get_cobweb_manifest(cobweb_entries);
+    update_manifest_if_changed("assets/cobweb/manifest.cob", manifest)
+        .expect("update_cobweb_manifest failed");
 }
--- cobble/src/lib.rs
+++ cobble/src/lib.rs
@@ -1,93 +1,120 @@
 use std::ffi::OsStr;
 use std::fs::{self, DirEntry};
-use std::path::PathBuf;
+use std::io;
+use std::path::{Path, PathBuf};
 
-type Line = (String, String);
+#[derive(Clone, Debug, Eq, PartialEq)]
+pub struct ManifestEntry {
+    path: PathBuf,
+    name: String,
+}
 
-pub fn cobble<P: Into<PathBuf>>(path: P, prefix: Option<String>) -> impl IntoIterator<Item = Line> {
-    let path: PathBuf = path.into();
-    if path.is_file() {
-        return Vec::new();
-    }
-
-    fn process_dir(entry: DirEntry, prefix: Option<String>) -> impl IntoIterator<Item = Line> {
-        let dir_name = entry
-            .path()
-            .file_name()
-            .unwrap()
-            .to_string_lossy()
-            .to_string();
-        let prefix = prefix
-            .map(|prefix| format!("{prefix}_{dir_name}"))
-            .unwrap_or(dir_name);
-        cobble(entry.path(), Some(prefix))
-    }
-
-    fn process_file(name: &OsStr, entry: DirEntry, prefix: Option<String>) -> Line {
-        let path = entry
-            .path()
-            .iter()
-            .skip(1)
-            .collect::<PathBuf>()
-            .to_string_lossy()
-            .to_string();
-        let name = name.to_string_lossy().to_string();
-        let name = prefix
-            .map(|prefix| format!("{prefix}_{name}"))
-            .unwrap_or(name);
-        (path, name)
-    }
-
-    fn process_entry(name: &OsStr, entry: DirEntry, prefix: Option<String>) -> Option<Vec<Line>> {
-        let ft = entry.file_type().ok()?;
-        if ft.is_file() && entry.path().extension().is_some_and(|ext| ext == "cob") {
-            Some(vec![process_file(name, entry, prefix)])
-        } else if ft.is_dir() {
-            let x = process_dir(entry, prefix);
-            Some(x.into_iter().collect())
-        } else {
-            None
+impl ManifestEntry {
+    fn new<P: Into<PathBuf>, S: ToString>(path: P, name: S) -> Self {
+        Self {
+            path: path.into(),
+            name: name.to_string(),
         }
     }
+    pub fn path(&self) -> &Path {
+        &self.path
+    }
+    pub fn name(&self) -> &str {
+        &self.name
+    }
+}
 
-    let entries = fs::read_dir(path)
-        .map(|entries| entries.flatten().collect())
-        .unwrap_or_else(|_| Vec::new());
+impl PartialOrd for ManifestEntry {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        self.path.partial_cmp(&other.path)
+    }
+}
 
-    entries
-        .into_iter()
-        .filter_map(|entry| {
+impl Ord for ManifestEntry {
+    fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+        self.path.cmp(&other.path)
+    }
+}
+
+fn process_dir(entry: DirEntry, prefix: Option<String>) -> io::Result<Vec<ManifestEntry>> {
+    let dir_name = entry
+        .path()
+        .file_name()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+    let prefix = prefix
+        .map(|prefix| format!("{prefix}_{dir_name}"))
+        .unwrap_or(dir_name);
+    get_cobweb_entries(entry.path(), Some(prefix))
+}
+
+fn process_file(name: &OsStr, entry: DirEntry, prefix: Option<String>) -> ManifestEntry {
+    let path = entry.path().iter().skip(1).collect::<PathBuf>();
+    let name = name.to_string_lossy();
+    let name = prefix
+        .map(|prefix| format!("{prefix}_{name}"))
+        .unwrap_or_else(|| name.to_string());
+    ManifestEntry::new(path, name)
+}
+
+fn process_entry(
+    name: &OsStr,
+    entry: DirEntry,
+    prefix: Option<String>,
+) -> io::Result<Vec<ManifestEntry>> {
+    let ft = entry.file_type()?;
+    if ft.is_file() && entry.path().extension().is_some_and(|ext| ext == "cob") {
+        let entry = process_file(name, entry, prefix);
+        Ok(vec![entry])
+    } else if ft.is_dir() {
+        process_dir(entry, prefix)
+    } else {
+        Ok(Vec::default())
+    }
+}
+
+pub fn get_cobweb_entries<P: Into<PathBuf>>(
+    root_dir: P,
+    prefix: Option<String>,
+) -> io::Result<Vec<ManifestEntry>> {
+    let root_dir: PathBuf = root_dir.into();
+    let entries = fs::read_dir(root_dir)?
+        .flatten()
+        .flat_map(|entry| {
             let path = entry.path();
-            let name = path.file_stem()?;
+            let name = path.file_stem().unwrap_or_default();
             process_entry(name, entry, prefix.clone())
         })
         .flatten()
-        .collect::<Vec<_>>()
+        .collect::<Vec<ManifestEntry>>();
+    Ok(entries)
 }
 
-pub fn generate_cobweb_manifest<P: Into<PathBuf>>(manifest_path: P) -> String {
-    let manifest_path: PathBuf = manifest_path.into();
-    let entries = cobble(
-        manifest_path.parent().expect("manifest must be in assets/"),
-        None,
-    )
-    .into_iter()
-    .collect::<Vec<_>>();
-
-    let mut names_to_import = entries
+pub fn get_cobweb_manifest<E: IntoIterator<Item = ManifestEntry>>(entries: E) -> String {
+    let mut entries = entries
         .into_iter()
         // do not include the manifest inside itself
-        .filter(|(_, name)| name != "manifest")
+        .filter(|entry| entry.name != "manifest")
         .collect::<Vec<_>>();
 
-    names_to_import.sort();
-    let imports = names_to_import
+    entries.sort();
+    let imports = entries
         .into_iter()
-        .map(|(path, name)| format!(r#""{path}" as {name}"#));
+        .map(|ManifestEntry { path, name }| format!(r#""{}" as {name}"#, path.to_string_lossy()));
 
     ["#manifest".to_string()]
         .into_iter()
         .chain(imports)
         .collect::<Vec<_>>()
         .join("\n")
+}
+
+pub fn generate_cobweb_manifest<P: Into<PathBuf>>(manifest_path: P) -> io::Result<String> {
+    let manifest_path: PathBuf = manifest_path.into();
+    get_cobweb_entries(
+        manifest_path.parent().expect("manifest must be in assets/"),
+        None,
+    )
+    .map(get_cobweb_manifest)
 }
